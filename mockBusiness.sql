102fe07747fce6c6de16c73b7ab03767a963b68c
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-05-28 11:36:48 -0700

    ensures proper syntax in the mockBussiness.sql file

--- mockBusiness.sql
+++ mockBusiness.sql
@@ -1,12 +1,17 @@
+DROP DATABASE IF EXISTS bussiness_db;
+CREATE DATABASE bussiness_db;
+
+\c bussiness_db;
+
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );  
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -16,18 +21,21 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     manager_id INT,
-    role_id INT
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );  
 
-    -- FOREIGN KEY (manager_id)
-    -- REFERENCES employees(id)
-    -- ON DELETE SET NULL,
-    -- FOREIGN KEY (role_id)
-    -- REFERENCES roles(id)
-    -- ON DELETE SET NULL
+
+ALTER TABLE employee 
+ADD FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL;
+    
     
     
 INSERT INTO department(name)
